f6bd5f3d54cfaf732d7eadc7522e545b8efb5725
Yuxuan Luo <luoyuxuan.carl@gmail.com>
2021-08-06 12:06:03 +0800

    Init Spring Boot framework

---
+++ src/main/java/com/tempVar/tempVarServer/TempVarServerApplication.java
@@ -0,0 +1,13 @@
+package com.tempVar.tempVarServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempVarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempVarServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tempVar/tempVarServer/TempVarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempVar.tempVarServer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class TempVarServerApplicationTests {
+
+	@Test(description = "idk")
+	void contextLoads() {
+	}
+
+}
